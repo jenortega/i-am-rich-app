082d571333eea0c3bccab74013c10ceb7e14e859
Jennefer Ortega <jennefereortega@gmail.com>
2021-01-14 20:48:26 -0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jennefer Ortega on 1/14/21.
+//  Copyright © 2021 Jennefer Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
